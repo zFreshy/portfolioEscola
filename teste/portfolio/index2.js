d7864fb13027539134ebc1f7c88d7d13f92379bd
zFreshy <91271353+zFreshy@users.noreply.github.com>
2025-01-06 03:12:42 -0300

    Add files via upload

--- teste/portfolio/index2.js
+++ teste/portfolio/index2.js
@@ -245,7 +245,36 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
     );
     
-  
+    // LENIS SMOOTH SCROLL
+    if (Webflow.env("editor") === undefined) {
+        lenis = new Lenis({
+            lerp: 0.1,
+            wheelMultiplier: 0.7,
+            gestureOrientation: "vertical",
+            normalizeWheel: false,
+            smoothTouch: false
+        });
+        function raf(time) {
+            lenis.raf(time);
+            requestAnimationFrame(raf);
+        }
+        requestAnimationFrame(raf);
+    }
+    $("[data-lenis-start]").on("click", function() {
+        lenis.start();
+    });
+    $("[data-lenis-stop]").on("click", function() {
+        lenis.stop();
+    });
+    $("[data-lenis-toggle]").on("click", function() {
+        $(this).toggleClass("stop-scroll");
+        if ($(this).hasClass("stop-scroll")) {
+            lenis.stop();
+        } else {
+            lenis.start();
+        }
+    });
+    
     
     
     
